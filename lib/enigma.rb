856327a07f6076c6462686634f1b8b9ef461ae4f
Darren Campbell <darren2802@gmail.com>
2019-11-03 11:42:36 -0700

    Adds to setup files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,19 @@
 class Enigma
 
+  def encrypt(message, key, date)
+    encrypted = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+  end
 
+  def decrypt(ciphertext, key, date)
+    {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,8 +1,32 @@
-require 'minitest/autorun'
-require '../lib/enigma'
+require_relative './test_helper'
+require_relative '../lib/enigma'
 
 class TestEnigma < Minitest::Test
 
+  def setup
+    @enigma = Enigma.new
+  end
 
+  def test_enigma_initialized
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_enigma_encrypt
+    encrypted = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+    assert_equal encrypted, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_enigma_decrypt
+    unencrypted = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal unencrypted, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 
 end
