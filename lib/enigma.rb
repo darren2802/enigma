2358a3f1d1d296ad7e5ff716f8a6f9a5ae410bfb
Darren Campbell <darren2802@gmail.com>
2019-11-03 20:52:13 -0700

    Adds encrypt and cipher functionality

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,48 @@
+require 'Date'
+require_relative './shift'
+
 class Enigma
 
-  def encrypt(message, key, date)
-    encrypted = {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: "040895"
-      }
+  def encrypt(message, key = generate_key, date = generate_date)
+    characters = message.chars
+    keys = keys(key)
+    offsets = offsets(date)
+    shifts = Shift.shifts(keys, offsets)
+    cipher(characters, shifts)
+  end
+
+  def cipher(characters, shifts)
+    shifted_chars = Shift.shift_chars(characters, shifts)
+    a_z_lookup = Shift.a_z_lookup
+    ciphered = ''
+    shifted_chars.each_value do |value|
+      if value[:encrypt?] == 'y'
+        ciphered += a_z_lookup.at(value[:index] % 27)
+      else
+        ciphered += value[:index]
+      end
+    end
+    ciphered
+  end
+
+  def generate_key
+    rand.to_s[2..6]
+  end
+
+  def generate_date
+    Date.today.strftime('%d%m%y')
+  end
+
+  def keys(key)
+    keys = []
+    key.chars.each_cons(2) { |char| keys << char.join }
+    keys.map { |key| key.to_i }
+  end
+
+  def offsets(date)
+    squared = (date.to_i ** 2).to_s
+    last_four = squared[-4..-1].chars
+    last_four.map { |offset| offset.to_i }
   end
 
   def decrypt(ciphertext, key, date)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require_relative './test_helper'
 require_relative '../lib/enigma'
+require 'Date'
 
 class TestEnigma < Minitest::Test
 
@@ -11,16 +12,28 @@ class TestEnigma < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_enigma_generate_key
+    srand(1111)
+    assert_equal '09554', @enigma.generate_key
+  end
+
+  def test_enigma_generate_date
+    date_today = Date.today.strftime('%d%m%y')
+    assert_equal date_today, @enigma.generate_date
+  end
+
+  def test_enigma_cipher
+    characters = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    shifts = [3, 27, 73, 20]
+    assert_equal "keder ohulw", @enigma.cipher(characters, shifts)
+  end
+
   def test_enigma_encrypt
-    encrypted = {
-        encryption: "keder ohulw",
-        key: "02715",
-        date: "040895"
-      }
-    assert_equal encrypted, @enigma.encrypt("hello world", "02715", "040895")
+    assert_equal "keder ohulw", @enigma.encrypt("hello world", "02715", "040895")
   end
 
   def test_enigma_decrypt
+    skip
     unencrypted = {
       decryption: "hello world",
       key: "02715",
